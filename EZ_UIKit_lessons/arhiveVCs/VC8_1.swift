ac0b47654c41bd55cb7ab54a92dba388e9d5a985
Sergey <s.s.r@mail.ru>
2018-03-23 23:51:21 +0300

    starting Task 8

---
+++ EZ_UIKit_lessons/arhiveVCs/VC8_1.swift
@@ -0,0 +1,20 @@
+//
+//  VC8_1ViewController.swift
+//  EZ_UIKit_lessons
+//
+//  Created by 1234 on 23.03.2018.
+//  Copyright © 2018 Sergey Riabinin. All rights reserved.
+//
+
+import UIKit
+
+class VC8_1: MyVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+
+}
